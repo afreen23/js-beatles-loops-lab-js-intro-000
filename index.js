d28a4eff39cbc29913036966a46fbc7e48c9b715
Soha Rahman <studentinsha12@gmail.com>
2017-11-06 07:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function theBeatlesPlay(musicians_array,instruments_array) {
   var array=[];
   for(var i=0;i<musicians_array.length;i++) {
     var s=`${musicians_array[i]} plays ${instruments_array[i]}`;
+    array.push(s);
     
   }
+  return array;
 }
